083d267c304be151492f1f3f196fdf26babc23f2
Andrey Pyankov <pyankoff@users.noreply.github.com>
2015-09-18 23:12:39 +0700

    fix public init in UICollectionViewCell

--- NibDesignable.swift
+++ NibDesignable.swift
@@ -142,7 +142,7 @@ public class NibDesignableControl: UIControl {
 public class NibDesignableCollectionViewCell: UICollectionViewCell {
     
     // MARK: - Initializer
-    override init(frame: CGRect) {
+    override public init(frame: CGRect) {
         super.init(frame: frame)
         self.setupNib()
     }
