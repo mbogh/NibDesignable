b3a8dbead10032f872caf565c3f0dc60b1d68375
Cihat Gündüz <commits@cihatguenduez.de>
2019-03-13 11:42:57 +0100

    Add basic SwiftPM manifest file

---
+++ Package.swift
@@ -0,0 +1,15 @@
+// swift-tools-version:4.2
+import PackageDescription
+
+let package = Package(
+    name: "NibDesignable",
+    products: [
+        .library(name: "NibDesignable", targets: ["NibDesignable"])
+    ],
+    targets: [
+        .target(
+            name: "NibDesignable",
+            path: "NibDesignable.swift"
+        )
+    ]
+)
