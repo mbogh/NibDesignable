99a2b46d206bc249a7856ca8ec027046e710384c
Cihat Gündüz <cihat.guenduez@jamitlabs.com>
2019-03-15 14:43:22 +0100

    Delete Demo project to fix issue with Carthage builds

--- NibDesignableDemo/Pods/Target Support Files/NibDesignable/NibDesignable-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_NibDesignable : NSObject
-@end
-@implementation PodsDummy_NibDesignable
-@end
--- NibDesignableDemo/Pods/Target Support Files/NibDesignable/NibDesignable-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double NibDesignableVersionNumber;
-FOUNDATION_EXPORT const unsigned char NibDesignableVersionString[];
-
